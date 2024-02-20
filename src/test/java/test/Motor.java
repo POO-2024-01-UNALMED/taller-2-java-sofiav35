f4561907042dfff7181887f4ac6bf849938aec1c
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-02-19 19:16:31 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,28 +1,20 @@
 package test;
-
 public class Motor {
-	
 	int numeroCilindros;
-    String tipo;
-    int registro;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int registro)
+	{
+		this.registro = registro;
+	}
+	
+	void asignarTipo(String tipo)
+	{
+		if(tipo == "electrico" || tipo == "gasolina")
+		{
+			this.tipo = tipo;
+		}
+	}
 
-   public Motor(int numeroCilindros, String tipo, int registro) {
-       this.numeroCilindros = numeroCilindros;
-       this.tipo = tipo;
-       this.registro = registro;
-   }
-
-   public void cambiarRegistro(int nuevoRegistro) {
-       this.registro = nuevoRegistro;
-   }
-   
-   public void asignarTipo(String tipo) {
-       if (tipo.equals("electrico") ) {
-           this.tipo = tipo;
-        
-       } 
-       else if(tipo.equals("gasolina")) {
-       	this.tipo=tipo;
-       }
-     }
 }
